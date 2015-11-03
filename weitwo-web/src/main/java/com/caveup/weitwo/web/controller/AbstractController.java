130fdd5180b3b21dc8d60b956b9639b7b3b43a50
xliangwu <xliangwu@gmail.com>
2015-11-04 00:17:02 +0800

    support web

---
+++ weitwo-web/src/main/java/com/caveup/weitwo/web/controller/AbstractController.java
@@ -0,0 +1,12 @@
+package com.caveup.weitwo.web.controller;
+
+import java.io.Serializable;
+
+public abstract class AbstractController implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ weitwo-web/src/main/java/com/caveup/weitwo/web/controller/MessageController.java
@@ -0,0 +1,10 @@
+package com.caveup.weitwo.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController extends AbstractController {
+
+    private static final long serialVersionUID = -22248706779491438L;
+
+}
